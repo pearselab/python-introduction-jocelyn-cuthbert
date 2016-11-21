37002ad05961c80746ab8db69771a22a4156572d
jocelyn-cuthbert <jcuthber@aggiemail.usu.edu>
2016-11-21 10:54:56 -0700

    learning new syntax and stuff and things

---
+++ intro.py
@@ -0,0 +1,31 @@
+###1### Write loop that prints out the numbers from 20 to 10
+for i in range (10,20,1):
+    print (i)
+#Gave me nothing yet.....I think that because I am starting at 20, and adding positive one
+    for i in range (10,20,-1):
+    print (i)
+#This worked correctly!  Yay!
+    
+###2### Write a list comprehension that returns the numbers form 20 to 10
+results = []
+for each in range (5, 10, 1):
+    result append(each*2)
+quicker_result = [each *2 for each in range(5,10)]
+
+results = []
+for each in range (20, 10, -1):
+    print (result)
+    
+
+###3### Write a loop that prints out only the numbers from 20 to 10 that are even.
+for i in range(20, 10, -2):
+    print(i)
+#This works but doesn't give me ten.  So I think that I need to go smaller than ten....
+for i in range(20, 9, -2):
+    print(i)
+#Bam.  Even numbers.
+
+#4. Write a list comprehension that prints out only the numbers from 20 to 10 that are even
+
+#5. Write a function that calculates whether a number is a prime number
+    
